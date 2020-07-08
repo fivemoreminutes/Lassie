7741d4766b50f50f828577086b91280da3609e35
Thomas Corbett <thomas.corbett.jr@gmail.com>
2020-07-08 16:25:27 -0400

    Changed names

--- Lassie/target/debug/deps/Lassie.d
+++ Lassie/target/debug/deps/Lassie.d
@@ -1,5 +1,5 @@
-C:\Users\tcorb\Documents\Lassie\Code\Lassie\Lassie\target\debug\deps\Lassie.exe: src\main.rs
+C:\Users\tcorb\Documents\Lassie\Code\Lassie\lassie\target\debug\deps\lassie.exe: src\main.rs
 
-C:\Users\tcorb\Documents\Lassie\Code\Lassie\Lassie\target\debug\deps\Lassie.d: src\main.rs
+C:\Users\tcorb\Documents\Lassie\Code\Lassie\lassie\target\debug\deps\lassie.d: src\main.rs
 
 src\main.rs:
