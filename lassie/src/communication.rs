0a36870f5c58ee9a5c90eb828e35ad75fd3c6ea2
Thomas Corbett <thomas.corbett.jr@gmail.com>
2020-07-20 00:05:31 -0400

    added function to allow data interpretation from u8 to f32 array

--- lassie/src/communication.rs
+++ lassie/src/communication.rs
@@ -1,5 +1,6 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
+use byteorder::{ByteOrder, LittleEndian};
 /* 
 let mut Address: String = "127.0.0.1".to_owned();
 let port: &str = ":88888";
@@ -14,28 +15,48 @@ pub fn send_data( address: & String, data: [f32; 4]  ) -> std::io::Result<()> {
     Ok(())
 }
 
-
 pub fn recieve_data(address: & String) -> std::io::Result<()> {
     let mut stream = TcpStream::connect(address)?;
     let mut buffer = Vec::new();
     stream.read_to_end(&mut buffer)?;
+    let mut data = to_f32_array(&buffer);
     Ok(())
 }
 
-pub fn test() {
-    println!("test");
-}
 
 fn to_u8<'a>(data: [f32;4]) -> &'a [u8] {
     let mut buffer = data.to_vec();
     let p = buffer.as_mut_ptr();
     let len = buffer.len();
-    let cap = buffer.capacity();
+    //let cap = buffer.capacity();
 
     unsafe{
         std::slice::from_raw_parts(p as *mut _, len)
 }
 }
 
+fn to_f32_array(buffer: &Vec<u8>) -> [f32;4]{
+    let len = buffer.len();
+    let mut done = false;
+    let mut array: [f32; 4] = [0.0; 4];
+    let mut i = 0;
+    let mut j = 4;
+
+    while !done {
+
+        array[i] = LittleEndian::read_f32(&buffer[i..=4]);
+        i = i+4;
+        j = j+4;
+        if i == len {
+            done = true;
+        }
+
+}
+array
+}
+
+pub fn test() {
+    println!("test");
+}
 
 
