3e1c399fb8f52c49a6599ef68f75d376edeb80d5
Thomas Corbett <thomas.corbett.jr@gmail.com>
2020-07-19 22:36:19 -0400

    Fixed send data by adding a converter to u8 type - untested

--- lassie/src/communication.rs
+++ lassie/src/communication.rs
@@ -5,19 +5,15 @@ let mut Address: String = "127.0.0.1".to_owned();
 let port: &str = ":88888";
 
 Address.push_str(port); */
-/*
-pub fn send_data( Address: & String, data: &mut  [f32) -> std::io::Result<()> {
-    let mut stream = TcpStream::connect(Address)?;
-    let mut pos = 0;
 
-    while pos <data.len(){
-        let bytes_written = stream.write(&data[pos..])?;
-        pos += bytes_written;
-    }
+pub fn send_data( address: & String, data: [f32; 4]  ) -> std::io::Result<()> {
+    let mut stream = TcpStream::connect(address)?;
+    let buf = to_u8(data);
+    stream.write_all(buf)?;
 
     Ok(())
 }
-*/
+
 
 pub fn recieve_data(address: & String) -> std::io::Result<()> {
     let mut stream = TcpStream::connect(address)?;
@@ -29,4 +25,17 @@ pub fn recieve_data(address: & String) -> std::io::Result<()> {
 pub fn test() {
     println!("test");
 }
+
+fn to_u8<'a>(data: [f32;4]) -> &'a [u8] {
+    let mut buffer = data.to_vec();
+    let p = buffer.as_mut_ptr();
+    let len = buffer.len();
+    let cap = buffer.capacity();
+
+    unsafe{
+        std::slice::from_raw_parts(p as *mut _, len)
+}
+}
+
+
 
