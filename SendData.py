d5265fc78f61f63ca7c3c78158c5af80b16af92c
Thomas Corbett <thomas.corbett.jr@gmail.com>
2020-07-25 00:07:29 -0400

    Untested Data sending/recieving from python end. Need to integrate server listening on Rust end

--- SendData.py
+++ SendData.py
@@ -1,14 +1,15 @@
-#temporary file that is currently incomplete
-
 import socket
 from struct import pack, unpack
 from bitstring import BitArray
 
-def comm_init(IP,Port):
+# Initiate communication with ras-pi at it's IP and port number
+
+
+def comm_init(IP, Port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(5)
-    try:
-        s.connect(IP,Port)
+    try:  # if there is an error it should return the error type to main
+        s.connect(IP, Port)
         return s
     except ConnectionError:
         return -1
@@ -16,31 +17,33 @@ def comm_init(IP,Port):
         return -2
 
 
+# These functions are egregiously untested and need to be checked
+
 def send_data(s, Data):
     l = len(Data)
     try:
         for x in range(l):
-            info += s.send(pack('f',Data[x]))
+            info += s.send(pack('f', Data[x]))
         return 1
     except ConnectionError:
         return -1
     except TimeoutError:
         return -2
-        
+
 
 def recieve_data(s, Data):
     BufferSize = 1024
     try:
-        Buffer= s.recv(BufferSize)
-        Data = unpack('f',Buffer)
+        Buffer = s.recv(BufferSize)
+        Data = unpack('f', Buffer)
         return Data
     except ConnectionError:
         return -1
 
+# This function is just here right now for future debugging of binary data if needed
+
+
 def binary_rep(Data):
-    A = pack('f',Data)
+    A = pack('f', Data)
     A = BitArray(A).bin
     return A
-
-
-
