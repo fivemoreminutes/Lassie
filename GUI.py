2116cf80bbe3e46617de6e3aa54d4a43b00c1d28
Thomas Corbett <thomas.corbett.jr@gmail.com>
2020-09-04 21:34:24 -0400

    Added a scrollbar widget to the GUI window to push messages to instead of the terminal

--- GUI.py
+++ GUI.py
@@ -1,9 +1,14 @@
 import tkinter as tk
+import tkinter.scrolledtext as st
 import SendData as SD
 import time 
 CONNECTION_ERROR = "Not Connected to Pi, cannot send data"
 addr = "192.168.0.3"
 port = 2000        
+SCRIPT = "[GUI] "
+INFO = "[INFO] "
+WARNING = "\n[WARNING] "
+
 class Application(tk.Frame):
     def __init__(self, master=None): #initializes the window
         super().__init__(master)
@@ -15,7 +20,7 @@ class Application(tk.Frame):
         self.text_color.set("red")
         self.configure(bg = 'snow3')
         self.net = SD.Network(addr,port)
-        for x in range(1,4):
+        for x in range(1,11):
             tk.Grid.columnconfigure(self,x,weight = 1)
             tk.Grid.rowconfigure(self,x,weight = 1) 
         
@@ -36,49 +41,91 @@ class Application(tk.Frame):
                                         command =self.disconnect, width = 20)
         self.connection_status = tk.Label(self, textvariable = self.text1, fg = self.text_color.get(),
                                          width = 20)
+        self.output_term = st.ScrolledText(self, wrap = tk.WORD, width = 40, height = 10)
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy)
         
         self.quit.grid(row = 0, column = 0)
-        self.right_arrow.grid(row = 2, column = 3, sticky = 'nswe')
-        self.left_arrow.grid(row = 2, column = 1, sticky = 'nswe')
-        self.up_arrow.grid(row = 1, column = 2, sticky = 'nswe')
-        self.down_arrow.grid(row = 3, column = 2, sticky = 'nswe')
-        self.test_button.grid(row=2, column = 2, sticky = 'nswe')
-        self.connect_button.grid(row = 0, column = 3, sticky = 'nswe')
-        self.disconnect_button.grid(row = 0, column = 2, sticky = 'nswe')
-        self.connection_status.grid(row = 0, column = 1, sticky = 'nswe')
+        self.right_arrow.grid(row = 4, column = 5, sticky = 'nswe', rowspan = 2, columnspan = 2)
+        self.left_arrow.grid(row = 4, column = 1, sticky = 'nswe', rowspan = 2, columnspan = 2)
+        self.up_arrow.grid(row = 2, column = 3, sticky = 'nswe', rowspan = 2, columnspan = 2)
+        self.down_arrow.grid(row = 6, column = 3, sticky = 'nswe', rowspan = 2, columnspan = 2)
+        self.test_button.grid(row=4, column = 3, sticky = 'nswe', rowspan = 2, columnspan = 2)
+        self.connect_button.grid(row = 0, column = 7, sticky = 'nswe',columnspan = 3)
+        self.disconnect_button.grid(row = 0, column = 4, sticky = 'nswe', columnspan = 3)
+        self.connection_status.grid(row = 0, column = 1, sticky = 'nswe', columnspan = 3)
+        self.output_term.grid(row = 1, column = 8, sticky = 'nswe',columnspan = 2, rowspan = 9)
 
     def up_arrow_press(self):
+        
         if self.net.connection == True:
-            print("Up Arrow Pressed")
+            message = "Up Arrow Pressed"
+            output = INFO + SCRIPT + message + "\n"
+            self.output_term.insert(tk.INSERT, output)
+            self.output_term.see(tk.END)
+
             self.net.sdata[1] = 3.2
         else:
-            print(CONNECTION_ERROR)
+            message = CONNECTION_ERROR
+            output = WARNING + SCRIPT + message + "\n"
+            self.output_term.insert(tk.INSERT, output)
+            self.output_term.see(tk.END)
 
     def down_arrow_press(self):
+
         if self.net.connection == True:
-            print("Down Arrow Pressed")
+            message = "down Arrow Pressed"
+            output = INFO + SCRIPT + message + "\n" 
+            self.output_term.insert(tk.INSERT, output)
+            self.output_term.see(tk.END)
+
             self.net.sdata[2] = 3.2
         else:
-            print(CONNECTION_ERROR)
+            message = CONNECTION_ERROR
+            output = WARNING + SCRIPT + message + "\n"
+            self.output_term.insert(tk.INSERT, output)   
+            self.output_term.see(tk.END)         
 
     def right_arrow_press(self):
+
         if self.net.connection == True:
-            print("right Arrow Pressed")
+            message = "right Arrow Pressed"
+            output = INFO + SCRIPT + message + "\n"
+            self.output_term.insert(tk.INSERT, output)
+            self.output_term.see(tk.END)
+
             self.net.sdata[3] = 3.2
         else:
-            print(CONNECTION_ERROR)
+            message = CONNECTION_ERROR
+            output = WARNING + SCRIPT + message + "\n"
+            self.output_term.insert(tk.INSERT, output)  
+            self.output_term.see(tk.END)          
+
 
     def left_arrow_press(self):
+
         if self.net.connection == True:
-            print("left Arrow Pressed")
+            message = "left Arrow Pressed"
+            output = INFO + SCRIPT + message + "\n"            
+            self.output_term.insert(tk.INSERT, output)
+            self.output_term.see(tk.END)
+
             self.net.sdata[4] = 3.2
         else:
-            print(CONNECTION_ERROR)
+            message = CONNECTION_ERROR
+            output = WARNING + SCRIPT + message + "\n"
+            self.output_term.insert(tk.INSERT, output)
+            self.output_term.see(tk.END)            
 
     def test_button_press(self): 
+
+        message = "Test Pressed"
+        output = INFO + SCRIPT + message + "\n"
+        self.output_term.insert(tk.INSERT, output)
+        self.output_term.see(tk.END)
+        
+        
         if self.net.sdata[0] > 3.0:
             self.net.sdata[0] = 0.01
         else:
@@ -89,23 +136,45 @@ class Application(tk.Frame):
             try:
                 self.net.comm_init()
                 if self.net.connection == True:
+                    label = "[INFO] "
+                    message = "Connected to Lassie"
+                    output = label + SCRIPT + message + "\n"
+
+                    self.output_term.insert(tk.INSERT, output)
+                    self.output_term.see(tk.END)
                     self.text1.set("Connection Status: Connected") 
                     self.connection_status.config(fg = "green")
             except:
-                print("There was an error in the connection")
+                label = "[WARNING] "
+                message = "There was an error connecting to Lassie"
+                output = label + SCRIPT + message + "\n"
+
+                self.output_term.insert(tk.INSERT, output)
+                self.output_term.see(tk.END)
+
     
     def disconnect(self):
         try:
             self.net.disconnect()
-            print("Connection closed successfully")
+
+            label = "[INFO] "
+            message = "Disconnected from Lassie"
+            output = label + SCRIPT + message + "\n"
+            self.output_term.insert(tk.INSERT, output)
+            self.output_term.see(tk.END)
+
             if self.net.connection == False:
                 self.text1.set("Connection Status: Not Connected") 
                 self.connection_status.config(fg = "red")
         except: 
-            print("There was an error in closing the connection")
+            label = "[INFO] "
+            message = "There was an error in closing the connection"
+            output = label + SCRIPT + message + "\n"
+            self.output_term.insert(tk.INSERT, output)
+            self.output_term.see(tk.END)
 
 root = tk.Tk()
-root.geometry("1000x900")
+root.geometry("1300x900")
 
 app = Application(master=root)
 
@@ -120,8 +189,10 @@ while True:
             app.net.data_exchange()
             print(app.net.rdata)
         except:
-            print("There was an error in sending data")
-    time.sleep(5*0.001)
-    
+            label = "[WARNING] "
+            message = "There was an error in data exchange"
+            output = label + SCRIPT + message + "\n"
 
-        
+            app.output_term.insert(tk.INSERT, output)
+            app.output_term.see(tk.END)
+    time.sleep(5*0.001)
--- lassie/src/movement/control.rs
+++ lassie/src/movement/control.rs
@@ -1,2 +1,28 @@
 //this code will be used to control the motion of the legs
 // this code should take in the inputs from the laptop and convert those to desired motor positions
+use crate::movement::legs;
+use crate::movement::legs::Legs;
+
+fn interpolate(leg: &mut Legs, input: Vec<f32>){
+    //this will break an input into several distinct steps inside of itself
+}
+
+fn set_height(leg: &mut Legs){
+    // this will control how tall the bot stands
+}
+
+fn twist(leg: &mut Legs) {
+    // this function will control the twist of the bot
+}
+
+fn pitch(leg: &mut Legs) {
+    // this function will control the pitch of the bot
+}
+
+fn correct(leg: &mut Legs) {
+    //this function will correct the bot based on orientation
+}
+
+fn calibrate(leg: &mut Legs) {
+    //this function will be run on startup to establish initial orientation conditions   
+}
